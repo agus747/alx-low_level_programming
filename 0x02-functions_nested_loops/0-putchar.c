d0a9ee8cede546236111d3a657abde8a2c614ba8
agus747 <otienoaugustine256@gmail.com>
2022-07-05 10:07:17 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - debugging example
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *putchar = "putchar";
+
+	while (*putchar)
+	{
+		_putchar(*putchar);
+			putchar++;
+	}
+	_putchar('\n');
+	return (0);
+}
